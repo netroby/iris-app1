d83287d746bceeae4f4d78ffcc7b461e91f170f0
ZhiFeng Hu <hufeng1987@gmail.com>
2016-06-12 15:25:22 +0800

    Update main.go
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/kataras/iris"
 
 func main() {
-	iris.Get("/o", func(c *iris.Context) {
+	iris.Get("/", func(c *iris.Context) {
 		c.Write("Hi %s", "iris")
 	})
 	iris.Listen(":8080")
